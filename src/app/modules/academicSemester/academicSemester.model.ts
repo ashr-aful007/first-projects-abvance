e58df053568d494968a58bdf8b05229a0bd27c51
ashr-aful007 <ashraful99980@gmail.com>
2023-12-19 19:41:49 +0600

    add pre hook for semester

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -34,6 +34,23 @@ const acdemicSemesterSchema = new Schema<TAcademicSemester>(
   },
 )
 
+
+acdemicSemesterSchema.pre('save', async function(next){
+      const isSemesterExists = await AcademicSemester.findOne({
+           name: this.name,
+           year: this.year
+      })
+
+      if(isSemesterExists){
+          throw new Error('Semester is already exists')
+      }
+      next()
+})
+
+
+
+
+
 export const AcademicSemester = model<TAcademicSemester>(
   'AcademicSemester',
   acdemicSemesterSchema,
