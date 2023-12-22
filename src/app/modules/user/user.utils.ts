5641106f49956c6485195a97e21d8e17d5175b0f
ashr-aful007 <ashraful99980@gmail.com>
2023-12-22 18:51:56 +0600

    add fixed Id generateProblem

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 import { TAcademicSemester } from "../academicSemester/academicSemester.interface";
 import { User } from "./user.model";
 
@@ -27,9 +28,25 @@ const findLastStudentId = async () =>{
 
   // yer semesterCode 4 digits number
  export const generateStudentId = async(payload: TAcademicSemester | null) =>{
-        const currentId = await findLastStudentId() ||  (0).toString()
-        let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
+         let currentId = (0).toString()
+
+
+      let lastStudentId = await findLastStudentId();
+      //2030 01 0001
+      const lastStudentSemesterCode = lastStudentId?.substring(4, 6) //01
+      const lastStudentYear = lastStudentId?.substring(0,4) //2030
+      const currentSemesterCode = payload?.code;
+      const currentYear = payload?.year;
+
+      //ch
+      if(lastStudentId && lastStudentSemesterCode === currentSemesterCode
+         && lastStudentYear === currentYear){
+           currentId = lastStudentId.substring(6)
+      }
+
+
+        let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
         incrementId = `${payload?.year}${payload?.code}${incrementId}`;
 
         return incrementId;
